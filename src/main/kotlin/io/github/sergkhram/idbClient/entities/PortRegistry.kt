a627f3729a12a83f1ca9f9ca2b083554f835d257
SKhramkov <hramkov@dev.vtb>
2022-10-11 19:26:26 +0200

    Beautify code p2

--- src/main/kotlin/io/github/sergkhram/idbClient/entities/PortRegistry.kt
+++ src/main/kotlin/io/github/sergkhram/idbClient/entities/PortRegistry.kt
@@ -1,7 +1,6 @@
 package io.github.sergkhram.idbClient.entities
 
 import java.io.IOException
-import java.net.DatagramSocket
 import java.net.ServerSocket
 
 
@@ -13,23 +12,15 @@ object PortRegistry {
 
     fun available(port: Int): Boolean {
         var ss: ServerSocket? = null
-        var ds: DatagramSocket? = null
         try {
             ss = ServerSocket(port)
             ss.reuseAddress = true
-            ds = DatagramSocket(port)
-            ds.reuseAddress = true
             return true
         } catch (_: IOException) {
         } finally {
-            ds?.close()
-            if (ss != null) {
-                try {
-                    ss.close()
-                } catch (e: IOException) {
-                    /* should not be thrown */
-                }
-            }
+            try {
+                ss?.close()
+            } catch (_: IOException) {}
         }
         return false
     }
