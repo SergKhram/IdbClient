376af44c6f8de26e6020a06dc3591fd3d460b2d5
SKhramkov <hramkov@dev.vtb>
2022-10-28 23:05:15 +0200

    Test impl - fixed simulator p11

--- src/test/kotlin/io/github/sergkhram/idbClient/CreateIdbClientTest.kt
+++ src/test/kotlin/io/github/sergkhram/idbClient/CreateIdbClientTest.kt
@@ -1,6 +1,9 @@
 package io.github.sergkhram.idbClient
 
+import idb.ConnectRequest
+import io.github.sergkhram.idbClient.entities.GrpcClient
 import io.github.sergkhram.idbClient.entities.address.TcpAddress
+import io.github.sergkhram.idbClient.entities.companion.RemoteCompanionData
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -12,7 +15,7 @@ class CreateIdbClientTest: BaseTest() {
     fun simpleCreateIdbClientTest() {
         runBlocking {
             val idb = IOSDebugBridgeClient()
-            val address = TcpAddress("127.0.0.1", 10882)
+            val address = TcpAddress("localhost", 10882)
             val udid = idb.connectToCompanion(address)
             log.info { "$udid - companion connected" }
             val list = idb.getTargetsList()
@@ -25,13 +28,26 @@ class CreateIdbClientTest: BaseTest() {
     @Test
     fun createIdbClientWCompanionInfoTest() {
         runBlocking {
-            val address = TcpAddress("127.0.0.1", 10882)
+            val address = TcpAddress("localhost", 10882)
             val idb = IOSDebugBridgeClient(
                 listOfCompanions = listOf(address)
             )
             val list = idb.getTargetsList()
             Assertions.assertEquals(1, list.size)
             Assertions.assertEquals(address, list.first().address)
+        }
+    }
+
+    @Test
+    fun connectCompanionTest() {
+        runBlocking {
+            val connectionResponse = GrpcClient(RemoteCompanionData(TcpAddress("localhost", 10882))).use {
+                it.stub.connect(
+                    ConnectRequest.getDefaultInstance()
+                )
+            }
+            val udid = connectionResponse.companion.udid
+            log.info { "$udid - companion connected" }
         }
     }
 }
