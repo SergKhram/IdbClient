698c116cbd62c018b06b12b41fe1f6eb22addfe2
SKhramkov <hramkov@dev.vtb>
2022-11-10 09:16:09 +0100

    Fixed exception message

--- src/main/kotlin/io/github/sergkhram/idbClient/util/CustomExceptions.kt
+++ src/main/kotlin/io/github/sergkhram/idbClient/util/CustomExceptions.kt
@@ -1,6 +1,6 @@
 package io.github.sergkhram.idbClient.util
 
 class NoCompanionWithUdidException(udid: String): Exception("There is no companion with $udid")
-class NoSimulatorsOnHostException(host: String): Exception("There is no simulators on this host: $host")
+class NoSimulatorsOnHostException(host: String): Exception("There are no simulators on this host: $host")
 class GetHostTargetsException(host: String, port: Int, error: String):
     Exception("Get host' targets process failed for $host:$port with reason: $error")
